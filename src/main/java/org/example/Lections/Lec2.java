18ab84c7dddcd3ad3a05f8576e6308b594306145
adachel <adachel74@gmail.com>
2025-01-30 17:52:14 +0500

    lec2 temp

---
+++ src/main/java/org/example/Lections/Lec2.java
@@ -0,0 +1,155 @@
+package org.example.Lections;
+
+public class Lec2 {
+    public Lec2() {
+    }
+
+    // пузырьковая сортировка
+    public void bubbleSort(int[] array){    // O(n^2)
+        boolean finish = true;
+        do {
+            finish = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]){
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    finish = false;
+                }
+            }
+        }while (!finish);
+    }
+
+    // сортировка выбором
+    public void directSort(int[] array){    // O(n^2)
+        for (int i = 0; i < array.length - 1; i++) {
+            int minPosition = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minPosition]){
+                    minPosition = j;
+                }
+            }
+            if (i != minPosition){
+                int temp = array[i];
+                array[i] = array[minPosition];
+                array[minPosition] = temp;
+            }
+        }
+    }
+
+    // сортировка вставками
+    public void insertSort(int[] array){    // O(n^2)
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]){
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+
+    // поиск значения в массиве
+    public int findNumber(int[] array, int value){  //O(n)
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value)
+                return i;
+        }
+        return -1;
+    }
+
+    // бинарный поиск
+    public int binarySearch(int[] array, int value, int min, int max){  // O(log(n))
+        int midPoint;
+        if (max < min){
+            return -1;
+        }else {
+            midPoint = (max - min) / 2 + min;
+        }
+
+        if (array[midPoint] < value){
+            return binarySearch(array, value, midPoint + 1, max);
+        }else {
+            if (array[midPoint] > value){
+                return binarySearch(array, value, min, midPoint - 1);
+            }else {
+                return midPoint;
+            }
+        }
+
+    }
+
+    // быстрая сортировка
+    public void quickSort(int[] array, int startPosition, int endPosition){    // O(nlog(n))
+        int leftPosition = startPosition;
+        int rightPosition = endPosition;
+        int pivot = array[(startPosition + endPosition) / 2];
+        do {
+            while (array[leftPosition] < pivot){
+                leftPosition++;
+            }
+            while (array[rightPosition] > pivot){
+                rightPosition--;
+            }
+            if (leftPosition <= rightPosition){
+                if (leftPosition < rightPosition){
+                    int temp = array[leftPosition];
+                    array[leftPosition] = array[rightPosition];
+                    array[rightPosition] = temp;
+                }
+                leftPosition++;
+                rightPosition--;
+            }
+        }while (leftPosition <= rightPosition);
+        if (leftPosition < endPosition){
+            quickSort(array, leftPosition, endPosition);
+        }
+        if (startPosition < rightPosition){
+            quickSort(array, startPosition, rightPosition);
+        }
+    }
+
+    // пирамидальная сортировка
+    public void heapSort(int[] array){
+        // построение кучи(перегруппируем массив)
+        for (int i = array.length / 2 - 1; i >= 0 ; i--) {
+            heapify(array, array.length, i);
+        }
+        // один за другим извлекаем элементы из кучи
+        for (int i = array.length - 1; i >= 0 ; i--) {
+            // перемещаем текущий корень в конец
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            // вызываем процедуру heapifi на уменьшенной куче
+            heapify(array, i, 0);
+        }
+    }
+    private void heapify(int[] array, int heapSize, int rootIndex){
+        int largest = rootIndex;    // инициализируем наибольший элемент как корень
+        int leftChild = 2 * rootIndex + 1; // левый
+        int rightChild = 2 * rootIndex + 2; // правый
+
+        // если левый дочерний элемент больше корня
+        if (leftChild < heapSize && array[leftChild] > array[largest]){
+            largest = leftChild;
+        }
+
+        // если правый дочерний элемент больше, чем самый большой элемент на данный момент
+        if (rightChild < heapSize && array[rightChild] > array[largest]){
+            largest = rightChild;
+        }
+
+        // если самый большой элемент не корень
+        if (leftChild != rootIndex){
+            int temp = array[rootIndex];
+            array[rootIndex] = array[largest];
+            array[largest] = temp;
+
+            // рекурсивно преобразуем в двоичную кучу затронутое поддерево
+            heapify(array, heapSize, largest);
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,10 @@
 package org.example;
 
 import org.example.Lections.Lec1;
+import org.example.Lections.Lec2;
 import org.example.Seminars.Sem1;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -18,7 +20,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 //        System.out.println("Задайте число");
 //        int num = Integer.parseInt(scanner.nextLine());
-
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //        Lec1 lec1 = new Lec1();
 //        System.out.println("Допустимые делители");
 //        System.out.println(lec1.findAvailableDivided(num));
@@ -35,8 +37,8 @@ public class Main {
 //        System.out.println(lec1.viewFib(num));
 //        System.out.println("Итераций в рекурсии: ");
 //        System.out.println(counter.get());
-
-        Sem1 sem1 = new Sem1();
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//        Sem1 sem1 = new Sem1();
 //        System.out.println("Сумма: " + sem1.sum(num));
 //        System.out.println("Простые числа: " + "\n" + sem1.simpleNumbers(num));
 //        System.out.println("Задайте число кубиков");
@@ -46,11 +48,25 @@ public class Main {
 
 //        System.out.println("Кол-во комбинаций вариант для 4: " + sem1.combinations1(n));
 //        System.out.println("Кол-во комбинаций вариант для пользовательского количества: " + sem1.combinations2(k, n));
-        System.out.println("Задайте число");
-        int num = Integer.parseInt(scanner.nextLine());
+//        System.out.println("Задайте число");
+//        int num = Integer.parseInt(scanner.nextLine());
 //        System.out.println("Фибоначчи числа " + num + " = " + sem1.fibo_rec(num));
 //        System.out.println("Фибоначчи числа " + num + " = " + sem1.fibo_line(num));
-        sem1.getTimeRecursion(num);
+//        sem1.getTimeRecursion(num);
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        Lec2 lec2 = new Lec2();
+        int[] myArray = new int[]{5, 3, 7, 1, 9, 2, 4, 6, 0, 8};
+//        lec2.bubbleSort(myArray);
+//        lec2.directSort(myArray);
+//        lec2.insertSort(myArray);
+//        lec2.quickSort(myArray, 0, myArray.length - 1);
+        lec2.heapSort(myArray);
+        System.out.println(Arrays.toString(myArray));
+        System.out.println(lec2.binarySearch(myArray, 7, 0, myArray.length - 1));
+
+
+
+
 
     }
 }
